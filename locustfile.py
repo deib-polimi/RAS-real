42c8caef65d9d79e7f42258e9efd3cc5a33a3407
gioenn <giovanniquattrocchi@me.com>
2022-12-15 16:15:54 +0100

    exp

--- locustfile.py
+++ locustfile.py
@@ -10,7 +10,7 @@ from locust import events
 from locust.runners import WorkerRunner
 
 
-EXP_FILE = "experiments/config/exp-ramp_static.json"
+EXP_FILE = "experiments/config/exp-ramp_qn.json"
 
 with open(EXP_FILE) as f:
     data = json.load(f)
