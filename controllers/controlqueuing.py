c9fab1155de5af2d5ef461339e50732b4506d4f7
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-12-16 09:02:47 +0100

    minor

--- controllers/controlqueuing.py
+++ controllers/controlqueuing.py
@@ -162,7 +162,7 @@ class OPTCTRL(Controller):
         
         print(rt,users, cores)
         if(t>self.esrimationWindow):
-            self.cores =max(self.OPTController(self.stime, self.setpoint, users, self.max_cores)+0.2*self.Ik, self.min_cores)
+            self.cores =max(self.OPTController(self.stime, self.setpoint, users, self.max_cores)+0.1*self.Ik, self.min_cores)
         else:
             self.cores=users[0]
     
