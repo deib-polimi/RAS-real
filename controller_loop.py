e9a9211e88167604e184aee1cf3851f8db5471f7
gioenn <giovanniquattrocchi@me.com>
2022-12-16 14:53:01 +0100

    fix

--- controller_loop.py
+++ controller_loop.py
@@ -12,7 +12,7 @@ containerSet = containerQuotas = None
 controller = None
 end = False
 
-allocatedCoreSet = allocatedCoreQuotas = 0 
+setCores = quotaCores = None
 
 @events.test_start.add_listener
 def on_locust_start(environment, **_kwargs):
@@ -27,27 +27,30 @@ def on_locust_start(environment, **_kwargs):
         end = True
 
 def controller_loop(environment):
+    global setCores, quotaCores
     shape = environment.shape_class
     sleep(1) # wait for some requests to be executed...
     while not end:
         t = shape.get_run_time()
         cores = controller.tick(t)
         setCores = min(int(cores), controller.max_cores-1)
-        quotasCores = max(controller.min_cores, cores-setCores)
+        quotaCores = max(controller.min_cores, cores-setCores)
         setCores = max(setCores, 1)
         print(f"{controller.name} - t: {int(t)} - cores: {cores} - RT: {controller.monitoring.getRT()} - users: {controller.monitoring.getUsers()}")
         containerSet.update(cpuset_cpus=f"{cpu_range_start}-{cpu_range_start+setCores-1}")
         if cores != setCores:
-            containerQuotas.update(cpu_quota=int(quotasCores*CPU_PERIOD), cpu_period=CPU_PERIOD)
+            containerQuotas.update(cpu_quota=int(quotaCores*CPU_PERIOD), cpu_period=CPU_PERIOD)
         sleep(controller.period)
 
         
 def setup(_controller, containerIds, _cpu_range_start):
-    global controller, containerSet, containerQuotas, cpu_range_start
+    global controller, containerSet, containerQuotas, cpu_range_start, setCores, quotaCores
     cpu_range_start = _cpu_range_start
     controller = _controller
     containerSet = client.containers.get(containerIds[0])
     containerQuotas = client.containers.get(containerIds[1])
     containerQuotas.update(cpuset_cpus=f"{cpu_range_start+controller.max_cores-1}")
+    setCores = 1
+    quotaCores = min(1, controller.min_cores)
 
 
