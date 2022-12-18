667807219b3bd9e9fc76d29138eedb5a0e0b038b
gioenn <giovanniquattrocchi@me.com>
2022-12-18 19:46:28 +0100

    exp-aws

--- locustfiles/exp-ramp_ct-aws-graph.py
+++ locustfiles/exp-ramp_ct-aws-graph.py
@@ -1,11 +1,11 @@
 CONFIG = {
     "hosts" : ["http://localhost:8080", "http://localhost:8081"],
-    "containerIds" : ["dynamic_set", "dynamic_quota"],
+    "containerIds" : ["graph_set", "graph_quota"],
     "request" : {
         "method" : "POST",
-        "data" : { "username": "dragonbanana", "random_len": 80000},
+        "data" : { "size": 25000},
         "headers" : {"Content-Type": "application/json"},
-        "path" : "/function/dynamic_html"
+        "path" : "/function/graph_mst"
     },
     "cpu_range_start" : 0,
     "monitoring_window": 30,
@@ -15,16 +15,18 @@ CONFIG = {
     "spawn_rate": 1,
     "end" : 600,
     "generator" : {
-        "class" : "WikiGen",
+        "class" : "RampGen",
         "params" : {
-        	"bias": 40,
-            "shift": 10,
+            "slope": 0.15,
+            "steady" : 450,
+            "initial" : 10,
+            "rampstart" : 150
         }
     },
     "controller" : {
         "class" : "CTControllerScaleX",
         "params" : {
-            "period" : 1, 
+            "period" : 3, 
             "init_cores" : 1, 
             "min_cores" : 0.5,
             "max_cores" : 16,
@@ -34,7 +36,6 @@ CONFIG = {
         }
     }
 }
-
 
 EXP_NAME = __file__.split("/")[-1].split(".")[0]
 
