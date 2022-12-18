293193593db4ce0cf43e542d68c3277d8c7305a9
gioenn <giovanniquattrocchi@me.com>
2022-12-18 00:26:15 +0000

    aws-exp

---
+++ aws-multi-runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the required number of arguments have been provided
+if [ $# -lt 3 ]
+then
+  echo "Usage: $0 REPETITIONS LOCUST_CPUS LOCUSTFILE1 LOCUSTFILE2 LOCUSTFILE3 ..."
+  exit 1
+fi
+
+N=$1
+CPU_RANGE=$2
+shift 2
+
+# Loop through all remaining arguments
+for file in "$@"
+do
+   ./aws-runner.sh 
+done
