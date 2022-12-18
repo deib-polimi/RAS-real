d8d7944ec25dcc0a4e48ead5abc5bcd3826ecd19
gioenn <giovanniquattrocchi@me.com>
2022-12-18 00:14:14 +0000

    aws-exp

--- aws-runner.sh
+++ aws-runner.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Check if the required number of arguments have been provided
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
-  echo "Usage: $0 FILE N"
+  echo "Usage: $0 LOCUST_FILE REPETITIONS LOCUST_CPUS"
   exit 1
 fi
 
 FILE=$1
 N=$2
+CPUS=$3
+
 
 # Repeat the commands N times
 for i in $(seq 1 $N)
 do
   git pull --rebase
-  taskset -c 25-31 locust --headless -f "$FILE"
+  echo $CPU_RANGE
+  echo $FILE
+  taskset -c $CPUS locust --headless -f $FILE
   git add -A
   git commit -am 'aws-exp'
   git push
