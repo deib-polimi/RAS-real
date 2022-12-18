7962d418f7110a226f9fcdf79732c4c049710f2a
gioenn <giovanniquattrocchi@me.com>
2022-12-18 22:11:05 +0100

    exp-aws

---
+++ utils/create_tex_table.py
@@ -0,0 +1,33 @@
+import os
+
+
+root_dir = 'experiments'
+workloads = ["sin", "ramp", "tweet", "wiki"]
+transforms_wl = [("SinGen", "SN3"), ("RampGen", "RP3"), ("TweetGen", "TW2"), ("WikiGen", "WK")]
+controllers = ["static", "rule-", "rule3", "step", "target-", "targetfast", "ct"]
+transforms_cnt = [ 
+    ("StaticController", "Static (1)"), 
+    ("RBControllerWithCooldown", "Rule-based"),
+    ("RBControllerWithCooldown", "Rule-based (+3)"),
+    ("StepController", "Step"),
+    ("TargetController", "Target"),
+    ("TargetController", "TargetFast"),
+    ("CTControllerScaleX", "ScaleX")    
+    ] 
+
+
+res = ""
+for workload, transform_wl in zip(workloads, transforms_wl):
+    for controller, transform_cnt in zip(controllers, transforms_cnt):
+        for root, dirs, files in os.walk(root_dir):
+            if  workload in root and controller in root and "aws" in root and "graph" not in root:
+                for file in files:
+                    if file.endswith('.tex'):
+                        with open(os.path.join(root, file), 'r') as f:
+                            res += f.read().replace(*transform_wl).replace(*transform_cnt) + "\n"
+                        break
+                break
+  
+
+with open("table.tex", "w") as f:
+    f.write(res)
