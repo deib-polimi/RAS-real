7cb671a358850bf523a17f07edd486e70b6fac91
gioenn <giovanniquattrocchi@me.com>
2022-12-18 00:07:28 +0000

    aws-exp

--- aws-runner2.sh
+++ aws-runner2.sh
@@ -11,19 +11,21 @@ FILE=$1
 N=$2
 CPUS=$3
 
-N=$1
-
 # Calculate the number of CPUs TOT available on the system
 TOT_CPUS=$(nproc)
+echo $TOT_CPUS
+echo $CPUS
 
 # Set the range for the taskset command
-CPU_RANGE=$(($TOT_CPUS-$N-1))-$(($TOT_CPUS-1))
+CPU_RANGE=$(($TOT_CPUS-$CPUS))-$(($TOT_CPUS-1))
 
 # Repeat the commands N times
 for i in $(seq 1 $N)
 do
   #git pull --rebase
-  echo 'taskset -c RANGE locust --headless -f "$FILE"'
+  echo $CPU_RANGE
+  echo $FILE
+  #taskset -c $CPU_RANGE locust --headless -f $FILE
   #git add -A
   #git commit -am 'aws-exp'
   #git push
