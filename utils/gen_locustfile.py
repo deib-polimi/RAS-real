7d9f2ed8a969384ce56e845ce8f812c267048c37
gioenn <giovanniquattrocchi@me.com>
2022-12-22 11:50:01 +0100

    exp-aws

--- utils/gen_locustfile.py
+++ utils/gen_locustfile.py
@@ -5,19 +5,23 @@ directory = "./locustfiles"
 
 replace_content = [
   (r'"controller"\s*:\s*\{[^}]*}[^}]*}', '''"controller" : {
-        "class" : "OPTCTRL",
+        "class" : "CTControllerScaleX",
         "params" : {
-            "period" : 1, 
-            "init_cores" : 1, 
+            "period" : 5, 
+            "init_cores" : 2, 
             "min_cores" : 0.5,
             "max_cores" : 16,
+            "BC" : 0.15, 
+            "DC" :  0.25, 
             "st" : 0.7
         }
     }''')
   ]
 
-filematches = ["ct", "aws"]
-replace_name = ("ct", "qn")
+replace_name = []
+
+filematches = ["ct", "aws", "graph"]
+
 
 
 for filename in os.listdir(directory):
@@ -27,8 +31,9 @@ for filename in os.listdir(directory):
   
     for c in replace_content:
       contents = re.sub(*c, contents, flags=re.DOTALL)
-    
-    new_filename = filename.replace(*replace_name)
+    new_filename = filename
+    for n in replace_name:
+      new_filename = filename.replace(*n)
     with open(os.path.join(directory, new_filename), "w") as f:
       f.write(contents)
     
