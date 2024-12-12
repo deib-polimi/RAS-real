0506547f11ce3d88f708b67a0372f9bf2ab7284f
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2024-12-12 15:01:58 +0100

    fixing exp settings

--- controllers/controlqueuingrobust.py
+++ controllers/controlqueuingrobust.py
@@ -191,7 +191,7 @@ class OPTCTRLROBUST(Controller):
         print(f"## core={core},users={users},st={st},rtm={rtm}")
         Tpred=min([users/(1.0+st),core/st])
         pred=(users/Tpred)
-        if([users/(1.0+st),core/st].index(Tpred)==0)
+        if([users/(1.0+st),core/st].index(Tpred)==0):
             pred-=1
         noise=rtm-pred
         print(f"###pred={pred}; noise={noise};")
