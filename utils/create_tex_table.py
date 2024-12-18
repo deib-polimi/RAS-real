144847f52385d19887dab2215475195789b7c833
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2024-12-18 15:39:22 +0100

    grouped robust exp

--- utils/create_tex_table.py
+++ utils/create_tex_table.py
@@ -2,19 +2,26 @@ import os
 import sys
 
 graph = sys.argv[1] == "True"
-root_dir = 'experiments'
+root_dir = './experiments/robust_exp/'
 workloads = ["sin", "ramp", "tweet", "wiki"]
 transforms_wl = [("SinGen", "SN3"), ("RampGen", "RP3"), ("TweetGen", "TW2"), ("WikiGen", "WK")]
-controllers = ["static", "rule-", "rule3", "step", "target-", "targetfast", "ct", "qn"]
+#controllers = ["static", "rule-", "rule3", "step", "target-", "targetfast", "ct", "qn"]
+controllers = ["ct", "qn","robust"]
+# transforms_cnt = [ 
+#     ("StaticController", "Static (1)"), 
+#     ("RBControllerWithCooldown", "Rule-based"),
+#     ("RBControllerWithCooldown", "Rule-based (+3)"),
+#     ("StepController", "Step"),
+#     ("TargetController", "Target"),
+#     ("TargetController", "TargetFast"),
+#     ("CTControllerScaleX", "\\approachCT"),
+#     ("OPTCTRL", "\\approachOPT")    
+#     ] 
+
 transforms_cnt = [ 
-    ("StaticController", "Static (1)"), 
-    ("RBControllerWithCooldown", "Rule-based"),
-    ("RBControllerWithCooldown", "Rule-based (+3)"),
-    ("StepController", "Step"),
-    ("TargetController", "Target"),
-    ("TargetController", "TargetFast"),
     ("CTControllerScaleX", "\\approachCT"),
-    ("OPTCTRL", "\\approachOPT")    
+    ("OPTCTRL", "\\approachOPT"),
+    ("OPTCTRLROBUST", "ROBUST")    
     ] 
 
 
@@ -26,7 +33,7 @@ for workload, transform_wl in zip(workloads, transforms_wl):
         postfix = ""
         last = controller == controllers[-1]
         for root, dirs, files in os.walk(root_dir):
-            if workload in root and controller in root and "aws" in root and ("graph" in root if graph else "graph" not in root):
+            if f"{workload}_{controller}" in root and "aws" in root and ("graph" in root if graph else "graph" not in root):
                 for file in files:
                     if file.endswith('.tex'):
                         with open(os.path.join(root, file), 'r') as f:
@@ -44,12 +51,12 @@ for workload, transform_wl in zip(workloads, transforms_wl):
                                 postfix = "\\\\"
                                 if last:
                                     postfix += " \hline"
-                                                           
                         break
+
         average_values = [sum(x)/len(x) for x in zip(*values)]
         average_values[4] = int(average_values[4])
         formatted_values = ['{:.2f}'.format(x) if isinstance(x, float) else str(x) for x in average_values]        
         res += prefix +' & '.join(formatted_values) + postfix + "\n"
 
-with open("table-dynamic.tex" if not graph else "table-graph.tex", "w") as f:
+with open("table-dynamic_robust.tex" if not graph else "table-graph.tex", "w+") as f:
     f.write(res)
