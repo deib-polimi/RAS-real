e6d8532d23ca0e96d0a01f822a343f928a95a30c
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-02-08 13:52:16 +0100

    added script for running app

---
+++ utils/start_graph_app.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+docker run --name graph_set -d  -p 8080:8080 systemautoscaler/sebs-dynamic_html:0.0.1 uwsgi --http 0.0.0.0:8080 --master -p 15 -w web_server_graph_mst:app
+docker run --name graph_quota -d  -p 8081:8080 systemautoscaler/sebs-dynamic_html:0.0.1 uwsgi --http 0.0.0.0:8080 --master -p 1 -w web_server_graph_mst:app
