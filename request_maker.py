9a0f711a9771749f9fc4143ddf492ffea8053893
emilio-imt <emilio-imt@Host-004.homenet.telecomitalia.it>
2025-02-09 15:19:00 +0100

    add noise to post parameter instead

--- request_maker.py
+++ request_maker.py
@@ -33,11 +33,14 @@ def setup(_monitoring, _controller, _hosts, _method, _headers, _data, _path,_noi
     noise_start = _noise_start
     noise_scale = _noise_scale
 
-def addNoise(rt,t):
-    noise = gauss(0, monitoring.sla * noise_scale)
-    if  noise_start !=None and t >= noise_start:
-        rt+=noise
-    return rt
+def addNoiseToSize(data, t):
+    if t >= noise_start:
+        original_size = data["size"]
+        noise = gauss(0, original_size * noise_scale)
+        data["size"] += noise
+        if data["size"] < 100:
+            data["size"] = 100
+    return data
 
 
 def run(task):
@@ -49,20 +52,22 @@ def run(task):
         c2 = controller_loop.quotaCores
         p = c1/(c1+c2)
         host = hosts[0] if random() <= p else hosts[1]
+    
+    t = env.shape_class.get_run_time()
 
     start = time.time()
     if method == "GET":
         task.client.get(host+path)
     elif method == "POST":
-        task.client.post(host+path, json=data, headers=headers)
+        #task.client.post(host+path, json=data, headers=headers)
+
+        json_data = data.copy()
+        json_data = addNoiseToSize(json_data, t)
+        task.client.post(host + path, json=json_data, headers=headers)
     end = time.time()
 
     rt = end - start
-    t = env.shape_class.get_run_time()
     users = env.runner.user_count
-
-    #aggiungo il rumore al tempo di risposta misurato
-    rt=addNoise(rt,t)
     
     monitoring.tick(t, rt, users, cores)
 
