a9b0248cf6f184f1334d8d0b681a2f7fd6d0cdac
emilio-imt <emilio-imt@Host-004.homenet.telecomitalia.it>
2025-02-09 21:36:02 +0100

    added a script for getting all the locust files

--- utils/getexp.sh
+++ utils/getexp.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 EXPDIR=$1
-export expfiles=$(ls $EXPDIR| grep -E '^exp-(tweet|ramp|sin|wiki)_(qn|ct|robust)-aws-graph\.py$' | while read -r file; do realpath "$file"; done)
+expfiles=$(ls $EXPDIR| grep -E '^exp-(tweet|ramp|sin|wiki)_(qn|ct|robust)-aws-graph\.py$' | while read -r file; do realpath "$EXPDIR/$file"; done)
+export expfiles
