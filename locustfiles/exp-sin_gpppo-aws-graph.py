b02f3177fa9404607f6ebe5c7348ed0b28d37bf2
emilio-imt <emilio-imt@MacBook-Pro-M1.homenet.telecomitalia.it>
2025-07-05 13:09:26 +0200

    refined GP guardrail for ppo controller

--- locustfiles/exp-sin_gpppo-aws-graph.py
+++ locustfiles/exp-sin_gpppo-aws-graph.py
@@ -13,7 +13,7 @@ CONFIG = {
     "wait_time_min": 1,
     "wait_time_max": 1,
     "spawn_rate": 1,
-    "end" : 600,
+    "end" : 1200,
     "noise_start":300,
     "noise_scale":1.0,
     "noise_type":"avg",
