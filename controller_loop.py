d8521fc69c9dbb02b8bd188671341cc0639c95c8
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-08 18:01:09 +0200

    debbugging set cores

--- controller_loop.py
+++ controller_loop.py
@@ -40,6 +40,7 @@ def controller_loop(environment):
         setCores = max(setCores, 1)
         print(f"{controller.name} - t: {int(t)} - cores: {cores} - RT: {controller.monitoring.getRT()} - users: {controller.monitoring.getUsers()}")
         containerSet.update(cpuset_cpus=f"{cpu_range_start}-{cpu_range_start+setCores-1}")
+        print(f"setCores: {setCores}, quotaCores: {quotaCores}")    
         if cores != setCores:
             containerQuotas.update(cpu_quota=int(quotaCores*CPU_PERIOD), cpu_period=CPU_PERIOD)
         sleep(controller.period)
