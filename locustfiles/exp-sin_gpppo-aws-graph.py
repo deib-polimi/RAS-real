6756ee4daa2adb4f1537cd9ab6d2e346a5441ae3
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-08 10:38:38 +0200

    refined gppo parameters

--- locustfiles/exp-sin_gpppo-aws-graph.py
+++ locustfiles/exp-sin_gpppo-aws-graph.py
@@ -30,7 +30,7 @@ CONFIG = {
         "params" : {
             "period" : 1, 
             "init_cores" : 1, 
-            "min_cores" : 28,
+            "min_cores" : 27,
             "max_cores" : 28,
             "st" : 0.8,
             "train" : False,
