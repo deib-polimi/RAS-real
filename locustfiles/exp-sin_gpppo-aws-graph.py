eda2d45688e84bdea0865a3604e48145310039cb
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-09 11:01:12 +0200

    debbugging set cores

--- locustfiles/exp-sin_gpppo-aws-graph.py
+++ locustfiles/exp-sin_gpppo-aws-graph.py
@@ -15,7 +15,7 @@ CONFIG = {
     "spawn_rate": 1,
     "end" : 800,
     "noise_start":150,
-    "noise_scale":2,
+    "noise_scale":2.3,
     "noise_type":"avg",
     "generator" : {
         "class" : "SinGen",
