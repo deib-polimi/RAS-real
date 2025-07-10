42cfe899c7b89e37a20233cc90eea8f58105e56b
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-10 21:55:43 +0200

    integrated scalex in gppo

--- locustfiles/exp-sin_gpppo-aws-graph.py
+++ locustfiles/exp-sin_gpppo-aws-graph.py
@@ -13,7 +13,7 @@ CONFIG = {
     "wait_time_min": 1,
     "wait_time_max": 1,
     "spawn_rate": 1,
-    "end" : 600,
+    "end" : 900,
     "noise_start":150,
     "noise_scale":2.0,
     "noise_type":"avg",
