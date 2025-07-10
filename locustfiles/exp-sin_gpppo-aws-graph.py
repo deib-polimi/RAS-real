85b8fd1755b2a193e824ccec997520798ff55746
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-10 20:54:06 +0200

    debbing gppo

--- locustfiles/exp-sin_gpppo-aws-graph.py
+++ locustfiles/exp-sin_gpppo-aws-graph.py
@@ -9,7 +9,7 @@ CONFIG = {
     },
     "cpu_range_start" : 1,
     "monitoring_window": 30,
-    "app_sla": 0.25,
+    "app_sla": 0.6,
     "wait_time_min": 1,
     "wait_time_max": 1,
     "spawn_rate": 1,
