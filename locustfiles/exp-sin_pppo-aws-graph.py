ce499df1c844bce02d08a5c4ff360aefe182f8e0
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-11 16:08:29 +0200

    integrated scalex in gppo

--- locustfiles/exp-sin_pppo-aws-graph.py
+++ locustfiles/exp-sin_pppo-aws-graph.py
@@ -15,7 +15,7 @@ CONFIG = {
     "spawn_rate": 1,
     "end" : 900,
     "noise_start":150,
-    "noise_scale":1.4,
+    "noise_scale":1.3,
     "noise_type":"avg",
     "generator" : {
         "class" : "SinGen",
