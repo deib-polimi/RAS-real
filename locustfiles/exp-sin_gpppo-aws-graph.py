73f028ebce0fe44c9dcc8e3e491e43ddc0709b6e
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-13 20:27:42 +0200

    added an automed tuning of st

--- locustfiles/exp-sin_gpppo-aws-graph.py
+++ locustfiles/exp-sin_gpppo-aws-graph.py
@@ -13,7 +13,7 @@ CONFIG = {
     "wait_time_min": 1,
     "wait_time_max": 1,
     "spawn_rate": 1,
-    "end" : 1800,
+    "end" : 2200,
     "noise_start":150, 
     "noise_scale":1.4,
     "noise_type":"avg",
