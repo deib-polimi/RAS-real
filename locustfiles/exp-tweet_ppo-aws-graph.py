21140bf4ce031ab83f52d8afd35420c042de9e22
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-07-17 23:01:41 +0200

    testing gppo and ppo on tweet load

--- locustfiles/exp-tweet_ppo-aws-graph.py
+++ locustfiles/exp-tweet_ppo-aws-graph.py
@@ -14,9 +14,9 @@ CONFIG = {
     "wait_time_min": 1,
     "wait_time_max": 1,
     "spawn_rate": 1,
-    "end" : 1200,
+    "end" : 2400,
     "noise_start":150, 
-    "noise_scale":1.8,
+    "noise_scale":1.5,
     "noise_type":"avg",
     "generator" : {
         "class" : "TweetGen",
